6c15585eae534cbbd83deca5809b3786a49ccaae
0liveira <trueoliveir4@gmail.com>
2017-09-29 18:59:01 -0300

    basic selenium script

---
+++ automacao.py
@@ -0,0 +1,19 @@
+#-*- coding:utf-8 -*-
+# pylint: disable=C0301
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+br = webdriver.Chrome('./chrome/chromedriver')
+wait = WebDriverWait(br, 10)
+
+def search(arg):
+    br.get('http://google.com.br')
+    wait.until(EC.visibility_of_element_located((By.XPATH, "//input[@name='q']")))
+    br.find_element_by_xpath("//input[@name='q']").send_keys(arg)
+    br.find_element_by_xpath("//input[@name='q']").send_keys(Keys.ENTER)
+
+for x in range(5):
+    search(x)
